5259b5706d39ff2da8a0559cef4f0e1a0274f72e
LUCIAJINGBENITO <luciajing.benito@alumnos.udit.es>
2026-01-12 18:27:03 +0100

    flecha

--- js/animations.js
+++ js/animations.js
@@ -389,5 +389,21 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
+  const scrollBtn = document.querySelector('.scroll-top');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+      scrollBtn.classList.add('show');
+    } else {
+      scrollBtn.classList.remove('show');
+    }
+  });
+
+  scrollBtn.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
 
 });
